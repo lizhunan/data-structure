cf95ec755446eac2d649dd7dd1a42bc1f366b0ff
lizhunan <384722830@qq.com>
2018-08-04 10:43:06 +0800

    1.添加线索二叉树及其算法2.添加Huffman树及其算法3.添加对于二叉树的非递归算法

--- data/tree.cpp
+++ data/tree.cpp
@@ -9,8 +9,7 @@ int main()
 {
 	BTNode *b;
 	CreateBTNode(b);
-	int d = LeafCount(b);
-	printf("%d", d);
+	PreOrderTraverse_(b);
 	return 0;
 }
 
@@ -53,6 +52,33 @@ void PreOrderTraverse(BTNode * b)
 	PreOrderTraverse(b->rchild);
 }
 
+void PreOrderTraverse_(BTNode * b)
+{
+	BTNode *St[50], *p;
+	int top = -1;
+	if (b != NULL)
+	{
+		top++; //根节点进栈
+		St[top] = b;
+		while (top > -1) //栈不为空时循环
+		{
+			p = St[top]; //退栈并访问该节点
+			top--;
+			printf("%c", p->data);
+			if (p->rchild != NULL) //右孩子节点进栈
+			{
+				top++;
+				St[top] = p->rchild;
+			}
+			if (p->lchild != NULL) //左孩子节点进栈
+			{
+				top++;
+				St[top] = p->lchild;
+			}
+		}
+	}
+}
+
 void InOrderTraverse(BTNode * b)
 {
 	if (b == NULL)
@@ -64,6 +90,32 @@ void InOrderTraverse(BTNode * b)
 	InOrderTraverse(b->rchild);
 }
 
+void InOrderTraverse_(BTNode * b)
+{
+	BTNode *St[50], *p;
+	int top = -1;
+	if (b != NULL)
+	{
+		p = b;
+		while (top > -1 || p != NULL) //栈不空或p不空时循环
+		{
+			while (p != NULL) //扫描*p的所有左下节点并进栈
+			{
+				top++;
+				St[top] = p;
+				p = p->lchild;
+			}
+			if (top > -1)
+			{
+				p = St[top]; //出栈*p节点
+				top--;
+				printf("%c", p->data);
+				p = p->rchild; //扫描*p的右孩子节点
+			}
+		}
+	}
+}
+
 void PostOrderTraverse(BTNode * b)
 {
 	if (b == NULL)
@@ -73,6 +125,41 @@ void PostOrderTraverse(BTNode * b)
 	PostOrderTraverse(b->lchild);
 	PostOrderTraverse(b->rchild);
 	printf("%c", b->data);
+}
+
+void PostOrderTraverse_(BTNode * b)
+{
+	BTNode *St[50], *p;
+	int flag, top = -1;
+	if (b != NULL)
+	{
+		do
+		{
+			while (b != NULL) //将*b的所有左下节点进栈
+			{
+				top++;
+				St[top] = b;
+				b = b->lchild;
+			}
+			p = NULL;
+			flag = 1;
+			while (top != -1 && flag)
+			{
+				b = St[top];
+				if (b->rchild == p)
+				{
+					printf("%c", b->data);
+					top--;
+					p = b;
+				}
+				else
+				{
+					b = b->rchild;
+					flag = 0;
+				}
+			}
+		} while (top != -1);
+	}
 }
 
 void LeveOrderTraverse(BTNode * b)
@@ -126,4 +213,105 @@ int TreeDeep(BTNode * b)
 		deep = leftDeep >= rightDeep ? leftDeep + 1 : rightDeep + 1;
 	}
 	return deep;
+}
+
+BiTTNodeP pre;/* 全局变量，始终指向刚刚访问过的节点 */
+void InThreading(BiTTNodeP b)
+{
+	if (b)
+	{
+		InThreading(b->lchild);
+		if (!b->lchild)
+		{
+			b->LTag = Thread;
+			b->lchild = pre;
+		}
+		if (!pre->rchild)
+		{
+			pre->RTag = Thread;
+			pre->rchild = b;
+		}
+		pre = b;
+		InThreading(b->rchild);
+	}
+}
+
+int InOrderTraverse_Thr(BiTTNodeP b)
+{
+	BiTTNodeP p;
+	p = b->lchild; //p指向根节点
+	while (p != b) //空树或遍历结束时，p==b
+	{
+		while (p->LTag == Link) //当LTag==0时循环到中序序列第一个节点
+		{
+			p = p->lchild;
+			printf("%c", p->data);
+			while (p->RTag == Thread && p->rchild != b)
+			{
+				p = p->rchild;
+				printf("%c", p->data);
+			}
+			p = p->rchild;
+		}
+	}
+	return 1;
+}
+
+void CreateHT(HTNode ht[], int n0)
+{
+	int i, j, k, lnode, rnode;
+	float min1, min2;
+	for (i = 0; i < 2 * n0 - 1; i++) //所有节点的相关域置初值-1
+	{
+		ht[i].parent = ht[i].lchild = ht[i].rchild = 1;
+	}
+	for (i = n0; i < 2 * n0 - 1; i++) //构造Huffman Tree
+	{
+		min1 = min2 = 32767; //lnode和rnode为最小权重的两个节点
+		lnode = rnode = -1;
+		for (k = 0; k < i - 1; k++)
+		{
+			if (ht[k].parent == -1)
+			{
+				if (ht[k].weight < min1)
+				{
+					min2 = min1; rnode = lnode;
+					min1 = ht[k].weight; lnode = k;
+				}
+				else if (ht[k].weight < min2)
+				{
+					min2 = ht[k].weight; rnode = k;
+				}
+			}
+			ht[lnode].parent = i; ht[rnode].parent = i;
+			ht[i].weight = ht[lnode].weight + ht[rnode].weight;
+			ht[i].lchild = lnode; ht[i].rchild = rnode;
+		}
+	}
+}
+
+void CreateHCode(HTNode ht[], HCode hcd[], int n0)
+{
+	int i, f, c;
+	HCode hc;
+	for (i = 0; i < n0; i++)
+	{
+		hc.start = n0;
+		c = i;
+		f = ht[i].parent;
+		while (f != -1)
+		{
+			if (ht[f].lchild == c)
+			{
+				hc.cd[hc.start--] = '0';
+			}
+			else
+			{
+				hc.cd[hc.start--] = '1';
+			}
+			c = f; f = ht[f].parent;
+		}
+		hc.start++;
+		hcd[i] = hc;
+	}
 }
--- data/tree.h
+++ data/tree.h
@@ -1,19 +1,57 @@
 char typedef ElemType;
+int typedef ElemType_Int;
+#define N 50
 
-// 二叉树（二叉链存储结构）
-typedef struct BinaryTreeNode 
+/* 二叉树（二叉链存储结构）*/
+typedef struct BinaryTreeNode
 {
 	ElemType data; //数据元素
 	struct BinaryTreeNode *lchild; //指向左孩子节点
 	struct BinaryTreeNode *rchild; //指向右孩子节点
 }BTNode;
 
+/* 二叉树的二叉线索书存储结构定义 */
+typedef enum { Link, Thread } PointerTag; //Link等于0表示指向左右孩子，Thread等于1表示指向前驱或后继线索
+typedef struct BinaryThreadTreeNode
+{
+	ElemType data;
+	struct BinaryThreadTreeNode *lchild; //指向左孩子节点
+	struct BinaryThreadTreeNode *rchild; //指向右孩子节点
+	PointerTag LTag; //左标志
+	PointerTag RTag; //右标志
+}BiTTNode, *BiTTNodeP;
+
+/* Huffman Tree 结构定义 */
+typedef struct HuffmanTreeNode
+{
+	ElemType_Int data; //节点值
+	float weight; //权重值 
+	int parent; //双亲节点
+	int lchild; //左孩子节点
+	int rchild; //右孩子节点
+}HTNode;
+
+/* Huffman编码 */
+typedef struct HuffmanCode
+{
+	char cd[N]; //存放当前节点的Huffman Code
+	int start; //起始位置
+
+}HCode;
+
 void CreateBTNode(BTNode *& bt); //二叉树（链式）建立（前序）
 void PreOrderTraverse(BTNode *b); //前序遍历（递归算法）
+void PreOrderTraverse_(BTNode *b); //前序遍历（非递归算法）
 void InOrderTraverse(BTNode *b); //中序遍历（递归算法）
+void InOrderTraverse_(BTNode *b); //中序遍历（非递归算法）
 void PostOrderTraverse(BTNode *b); //后序遍历（递归算法）
-void LeveOrderTraverse(BTNode *b); //层序遍历
+void PostOrderTraverse_(BTNode *b); //后序遍历（非递归算法）
+void LeveOrderTraverse(BTNode *b); //层序遍历（队列实现）
 int LeafCount(BTNode *b); //统计二叉树叶子节点个数
 int TreeDeep(BTNode *b); //二叉树深度（后序遍历）
+void InThreading(BiTTNodeP b); //中序遍历进行中序线索化
+int InOrderTraverse_Thr(BiTTNodeP b); //遍历线索树，中序遍历二叉线索树T
+void CreateHT(HTNode ht[], int n0); // 构造Huffman Tree
+void CreateHCode(HTNode ht[], HCode hcd[], int n0); //构造Huffman编码
 //////////////////////////////////////////
 
